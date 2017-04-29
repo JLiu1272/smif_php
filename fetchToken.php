c081f17afbfc678701e74bc88f8f7b8ab236b0af
JenniferLiu <jwt7689@rit.edu>
2017-04-29 12:12:35 -0400

    Testing whether fetchToken.php is working

--- fetchToken.php
+++ fetchToken.php
@@ -4,11 +4,14 @@
    session_start();
    header('Content-Type: application/json');
 
-   if($_SERVER['REQUEST_METHOD'] === 'POST'){
+   $query_id = $pdo->prepare("INSERT INTO TOKEN token_id) VALUES ('3321')");
+   $query_id->execute();
+
+   /*if($_SERVER['REQUEST_METHOD'] === 'POST'){
    		$token = $_POST["token"];
    		$query_id = $pdo->prepare("INSERT INTO TOKEN token_id) VALUES ('$token')");
    		$query_id->execute();
    		echo $token;
-   }
+   }*/
 
 ?>
