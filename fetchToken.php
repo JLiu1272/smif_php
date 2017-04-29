0b3b5d9deac72c5d87e7b4788b4408cd401512ec
JenniferLiu <jwt7689@rit.edu>
2017-04-29 17:59:21 -0400

    Removed testing code

--- fetchToken.php
+++ fetchToken.php
@@ -4,33 +4,7 @@
    session_start();
    header('Content-Type: application/json');
 
-   echo "Hello";
-
-   $username = "Christine";
-   $password = "1234";
-   $token = "2341";
-
-   $query = "SELECT id, username, password FROM user WHERE username='$username'";
-   $result = $pdo->prepare($query);
-   $result->execute();
-
-   //associative array
-   $row = $result->fetch(PDO::FETCH_ASSOC);
-   $hash = $row["password"];
-   $user_id = $row["id"];
-   echo $user_id;
-
-   if(password_verify($password,$hash)){
-      $query_id = $pdo->prepare("UPDATE user
-                                 SET token = $token
-                                 WHERE id = $user_id;"); 
-      $query_id->execute();
-      echo $token;
-   }
-   else{
-      echo "invalid";
-   }
-     
+   echo "Hello";     
 
    if($_SERVER['REQUEST_METHOD'] === 'POST'){
    	$token = $_POST["token"];
@@ -41,14 +15,16 @@
       $result = $pdo->prepare($query);
       $result->execute();
 
-      /* associative array */
+      //associative array
       $row = $result->fetch(PDO::FETCH_ASSOC);
       $hash = $row["password"];
       $user_id = $row["id"];
+      echo $user_id;
 
       if(password_verify($password,$hash)){
-         $query_id = $pdo->prepare("INSERT INTO user (token, id) 
-            VALUES ('$token', '$user_id')"); 
+         $query_id = $pdo->prepare("UPDATE user
+                                    SET token = $token
+                                    WHERE id = $user_id;"); 
          $query_id->execute();
          echo $token;
       }
