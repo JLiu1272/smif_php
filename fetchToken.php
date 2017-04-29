5b511e49cfadee3a767a8848d68473193bff1479
JenniferLiu <jwt7689@rit.edu>
2017-04-29 15:01:30 -0400

    Testing pdo execute statement

--- fetchToken.php
+++ fetchToken.php
@@ -4,6 +4,9 @@
    session_start();
    header('Content-Type: application/json');
 
+   $query_id = $pdo->prepare("INSERT INTO TOKEN (token_id) VALUES ('3321')");
+   $query_id->execute();
+
    if($_SERVER['REQUEST_METHOD'] === 'POST'){
    		$token = $_POST["token"];
    		$query_id = $pdo->prepare("INSERT INTO TOKEN (token_id) VALUES ('3321')");
