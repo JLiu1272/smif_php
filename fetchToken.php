d745a21934c4097f8e53ae1223660437343d4480
JenniferLiu <jwt7689@rit.edu>
2017-04-29 14:53:44 -0400

    Fixed syntax error in fetchToken.php

--- fetchToken.php
+++ fetchToken.php
@@ -4,14 +4,11 @@
    session_start();
    header('Content-Type: application/json');
 
-   $query_id = $pdo->prepare("INSERT INTO TOKEN token_id) VALUES ('3321')");
-   $query_id->execute();
-
-   /*if($_SERVER['REQUEST_METHOD'] === 'POST'){
+   if($_SERVER['REQUEST_METHOD'] === 'POST'){
    		$token = $_POST["token"];
-   		$query_id = $pdo->prepare("INSERT INTO TOKEN token_id) VALUES ('$token')");
-   		$query_id->execute();
+   		$query_id = $pdo->prepare("INSERT INTO TOKEN (token_id) VALUES ('3321')");
+         $query_id->execute();
    		echo $token;
-   }*/
+   }
 
 ?>
