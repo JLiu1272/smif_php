6c9cf7879ec858d6e455cd6995dd6a1d7835bc3f
JenniferLiu <jwt7689@rit.edu>
2017-04-29 17:50:05 -0400

    logic failed, testing where it failed

--- fetchToken.php
+++ fetchToken.php
@@ -6,28 +6,29 @@
 
    echo "Hello";
 
-   /*$username = "Christine";
-   $password = "1234";*/
+   $username = "Christine";
+   $password = "1234";
 
-   /*$query = "SELECT id, username, password FROM user WHERE username='$username'";
-      $result = $pdo->prepare($query);
-      $result->execute();
+   $query = "SELECT id, username, password FROM user WHERE username='$username'";
+   $result = $pdo->prepare($query);
+   $result->execute();
 
-      associative array
-      $row = $result->fetch(PDO::FETCH_ASSOC);
-      $hash = $row["password"];
-      $user_id = $row["id"];
+   //associative array
+   $row = $result->fetch(PDO::FETCH_ASSOC);
+   $hash = $row["password"];
+   $user_id = $row["id"];
+   echo $user_id;
 
-      if(password_verify($password,$hash)){
-         $query_id = $pdo->prepare("INSERT INTO user (token, id) 
-            VALUES ('$token', '$user_id')"); 
-         $query_id->execute();
-         echo $token;
-      }
-      else{
-         echo "invalid";
-      }
-   */   
+   if(password_verify($password,$hash)){
+      $query_id = $pdo->prepare("INSERT INTO user (token, id) 
+         VALUES ('$token', '$user_id')"); 
+      $query_id->execute();
+      echo $token;
+   }
+   else{
+      echo "invalid";
+   }
+     
 
    if($_SERVER['REQUEST_METHOD'] === 'POST'){
    	$token = $_POST["token"];
