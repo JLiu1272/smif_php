50042d03c505c496776196727eb8bdf1382d77f8
JenniferLiu <jwt7689@rit.edu>
2017-04-29 12:10:48 -0400

    Fixed syntax error in fetchToken.php

--- fetchToken.php
+++ fetchToken.php
@@ -6,10 +6,9 @@
 
    if($_SERVER['REQUEST_METHOD'] === 'POST'){
    		$token = $_POST["token"];
-   		$query_id = $pdo->prepare("INSERT INTO TOKEN token_id
-                       VALUES '$token';");
+   		$query_id = $pdo->prepare("INSERT INTO TOKEN token_id) VALUES ('$token')");
    		$query_id->execute();
+   		echo $token;
    }
-   echo "Hello";
 
 ?>
