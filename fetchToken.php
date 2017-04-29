159016401a0c2e2f3a1ed2cb48df801dadebf665
JenniferLiu <jwt7689@rit.edu>
2017-04-29 15:03:43 -0400

    pdo statement succeeded

--- fetchToken.php
+++ fetchToken.php
@@ -4,11 +4,12 @@
    session_start();
    header('Content-Type: application/json');
 
-   $query_id = $pdo->prepare("INSERT INTO TOKEN (token_id) VALUES ('3321')");
+   /*$query_id = $pdo->prepare("INSERT INTO TOKEN (token_id) VALUES ('3321')");
    $query_id->execute();
-   echo "Hello";
+   echo "Hello";*/
 
    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+      echo "Hello";
    	$token = $_POST["token"];
    	$query_id = $pdo->prepare("INSERT INTO TOKEN (token_id) VALUES ('$token')");
    	$query_id->execute();
